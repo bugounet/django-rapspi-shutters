daa90627d24fc664416bc0adfe3ee1fbe36ff46e
sullivan matas <sullivan@izberg-marketplace.com>
2018-07-06 09:47:07 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='Django RaspiShutters',
+    version='1.0',
+    packages=['raspi_shutters',],
+    license='Do what you want',
+    author="Sullivan MATAS",
+    author_email="sullivan@matas.pro",
+    description=("App to controll rolling shutters in django"),
+    keywords="shutters django controller",
+    url="https://github.com/bugounet/django-raspi-shutters",
+    long_description="This is a simple rolling shutter actuation django-app.",
+)
